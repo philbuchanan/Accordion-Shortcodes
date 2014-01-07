8cb9e332fe1dc55ecce3b65271da4d3934eb9560
Philip Buchanan <info@philbuchanan.com>
2014-01-07 16:01:00 -0500

    Check for accordion settings variable
    - sets the default settings if they aren’t loaded properly

--- accordion.js
+++ accordion.js
@@ -4,7 +4,16 @@
 	var allPanels  = $('.accordion > dd').hide(),
 		firstPanel = $('.accordion > dd:first-of-type'),
 		duration   = 250,
-		settings   = accordionSettings;
+		settings   = {
+			autoClose: true,
+			openFirst: false,
+			clickToClose: false
+		};
+	
+	// Check for accordion settings variable passed from WordPress
+	if (typeof accordionSettings !== 'undefined') {
+		settings = accordionSettings;
+	}
 	
 	// Open the first accordion item
 	if (settings.openFirst) {
