d2b432db7abdb40dedd0982305958f3194fdc685
Carly Gerard <carly.gerard@wwu.edu>
2019-03-18 09:18:52 -0700

    changed keydown functions to keyup for a11y

--- accordion.js
+++ accordion.js
@@ -74,7 +74,7 @@
 				clickControllerHandler(item);
 			});
 
-			$(this).keydown(function(event) {
+			$(this).keyup(function(event) {
 				var code = event.which;
 
 				// We only need to add manual keyboard events if _not_ using `<button>` elements
